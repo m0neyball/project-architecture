0e7ccdc89ac2f3e46061f50b3dde068672932396
ben <ben@moneyball.io>
2016-05-12 09:26:19 +0800

    Add todo_list action

---
+++ resources/assets/actions/todo_list.js
@@ -0,0 +1,12 @@
+import { TODO_LIST } from '../constants';
+
+let nextTodoId = 0;
+
+export const addTodo = (text) => {
+  const action = {
+    type: TODO_LIST.ADD_TODO,
+    id: nextTodoId++,
+    text
+  };
+  return action;
+};
