564fca58ca7ee2fbc1657d95e0f9184718fae5c3
ben <ben@moneyball.io>
2016-05-13 13:38:17 +0800

    Add ReduxThunk and callAPIMiddleware middleware for TodoList.jsx

--- resources/assets/entries/todo_list/index.jsx
+++ resources/assets/entries/todo_list/index.jsx
@@ -1,7 +1,10 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
 // reducers
 import todos from 'reducers/todos';
@@ -9,10 +12,16 @@ import todos from 'reducers/todos';
 // containers
 import TodoList from 'entries/todo_list/containers/TodoList.jsx';
 
+// middlewares
+import callAPIMiddleware from 'middlewares/call_API_middleware';
+
 const rootReducer = combineReducers({
   todos
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(
+  ReduxThunk,
+  callAPIMiddleware
+));
 
 class RootApp extends React.Component {
   render() {
