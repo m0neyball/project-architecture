100a2f9978673ccdaf0d4e8956ea777352f2df45
ben <ben@moneyball.io>
2016-05-13 13:34:12 +0800

    Add loadTodos action for redux

--- resources/assets/actions/todo_list.js
+++ resources/assets/actions/todo_list.js
@@ -1,4 +1,5 @@
 import { TODO_LIST } from 'constants';
+import fetch from 'isomorphic-fetch';
 
 let nextTodoId = 0;
 
@@ -10,3 +11,20 @@ export const addTodo = (text) => {
   };
   return action;
 };
+
+export function loadTodos() {
+  return {
+    // 要在之前和之後發送的 actions types
+    types: [
+      TODO_LIST.LOAD_TODOS_REQUEST,
+      TODO_LIST.LOAD_TODOS_SUCCESS,
+      TODO_LIST.LOAD_TODOS_FAILURE
+    ],
+    // 檢查快取 (可選擇的)：
+    shouldCallAPI: (state) => state.todos,
+    // 執行抓取資料：
+    callAPI: () => fetch('/todo', { method: 'GET' }),
+    // 要在開始/結束 actions 注入的參數
+    payload: {}
+  };
+}
