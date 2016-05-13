f6a1c8b777d2a4126e9d25c3965869f85b4f231c
ben <ben@moneyball.io>
2016-05-13 13:36:50 +0800

    Add LOAD_TODOS... for todos reducer

--- resources/assets/reducers/todos.js
+++ resources/assets/reducers/todos.js
@@ -2,6 +2,18 @@ import { TODO_LIST } from 'constants';
 
 const todos = (state = [], action) => {
   switch (action.type) {
+    case TODO_LIST.LOAD_TODOS_REQUEST: {
+      console.log('LOAD_TODOS_REQUEST');
+      return state;
+    }
+    case TODO_LIST.LOAD_TODOS_SUCCESS: {
+      return [...action.response.todos];
+    }
+    case TODO_LIST.LOAD_TODOS_FAILURE: {
+      console.log(action.response);
+      return state;
+    }
+
     case TODO_LIST.ADD_TODO: {
       const todo = {
         id: action.id,
